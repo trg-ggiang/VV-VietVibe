6daf4aa9938ccf3d92afb5de197cb1302b0486fc
Tungcso <luongtung882005@gmail.com>
2026-06-13 00:34:44 +0700

    m

--- my-frontend/app/page.tsx
+++ my-frontend/app/page.tsx
@@ -1,5 +1,11 @@
 import HomeScreen from "./_components/home-screen";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function Home() {
-  return <HomeScreen />;
+  return (
+    <ProtectedRoute>
+      <HomeScreen />
+    </ProtectedRoute>
+  );
 }
+
